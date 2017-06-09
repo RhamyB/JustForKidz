601603f951e16c63fce41c5ee4f150fb7cfadc29
Rhamy Belayachi <rhamy.belayachi@gmail.com>
2017-06-09 19:05:13 -0400

    Modified Project from Active links to Static Text

--- projects.php
+++ projects.php
@@ -22,34 +22,34 @@
 							<section class="posts">
 								<article>
 									<header>
-										<h2><a href="#">Arusha, Tanzania<br />
-										(2016-Present)</a></h2>
+										<h2>Arusha, Tanzania<br />
+										(2016-Present)</h2>
 									</header>
-									<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+									<span class="image fit"><img src="images/pic02.jpg" alt="" /></span>
 									<p>In efforts to expand our international impact and advance children's educational rights, Just For Kidz has selected Good Hope Orphanage as the beneficiary of our next project term. We are fundraising to build a secondary school (8th-12th grade) for 716 orphans and poor kids in the village so that they can escape the cycle of poverty and receive a college education. The orphanage will provide accomodations, shelter, and care for about 217 more orphans.</p>
 								</article>
 								<article>
 									<header>
-										<h2><a href="#">Port-au-Prince, Haiti<br />
-										(2014-15)</a></h2>
+										<h2>Port-au-Prince, Haiti<br />
+										(2014-15)</h2>
 									</header>
-									<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+									<span class="image fit"><img src="images/pic03.jpg" alt="" /></span>
 									<p>Just For Kidz collected clothing and shoes to donate to an orphanage in Port-au-Prince that helps about 30 boys and girls. Collecting a total of 4 boxes of clothes and shoes, the Founder personally delivered the clothes and shoes and distributed it among the children as their Christmas presents!</p>
 								</article>
 								<article>
 									<header>
-										<h2><a href="#">Chennai, India<br />
-										(2011-16)</a></h2>
+										<h2>Chennai, India<br />
+										(2011-16)</h2>
 									</header>
-									<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
+									<span class="image fit"><img src="images/pic04.jpg" alt="" /></span>
 									<p>Just For Kidz held bake sales, garage sales, jewelry sales, and talent shows at local community events to fundraise a one-month supply of medication for children with spastic needs at The Spastics Society of Tamilnadu.<br />Just For Kidz is currently working on a mini-project to fundraise for a sewing machine and other basic utilities for Aalayam, an orphanage that provides for 19 orphan boys and girls.</p>
 								</article>
 								<article>
 									<header>
-										<h2><a href="#">Walajapet, India<br />
-										(2010-11)</a></h2>
+										<h2>Walajapet, India<br />
+										(2010-11)</h2>
 									</header>
-									<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
+									<span class="image fit"><img src="images/pic05.jpg" alt="" /></span>
 									<p>Just For Kidz fundraised $1,200 dollars to change the asbestos roof of Dheenabandu Ashram, a small orphanage that houses, teaches, and protects about 200 orphans and poor children in the village. The leaky roof prevented the students from attending class, caused heat stroke, and put them at risk for mesothelioma. Volunteers and donations completely maintain the orphanage, as the government only grants 10 cents per child per day.</p>
 								</article>
 							</section>

7a21f566711f8a76f9d4a892badb798de902c455
Rhamy Belayachi <rhamy.belayachi@gmail.com>
2017-06-21 23:00:18 -0400

    Mailing List Integration

--- footer.php
+++ footer.php
@@ -1,7 +1,60 @@
 <?php echo'<!-- Footer -->
 					<footer id="footer">
-						<section>
-							
+						<section style="margin: 10px;">
+							<!-- Begin MailChimp Signup Form -->
+							<form action="//jfkorg.us16.list-manage.com/subscribe/post?u=ffec1f87c2fcc1d783cb8f946&amp;id=21d1c25c56" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							    <div id="mc_embed_signup_scroll">
+								<h2>Subscribe to our mailing list</h2>
+							<div class="field">
+								<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+							</label>
+								<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+							</div>
+							<br />
+							<div class="field">
+								<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+							</label>
+								<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+							</div>
+							<br />
+							<div class="field">
+								<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+							</label>
+								<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+							</div>
+							<br />
+								<div id="mce-responses" class="clear">
+									<div class="response" id="mce-error-response" style="display:none"></div>
+									<div class="response" id="mce-success-response" style="display:none"></div>
+								</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ffec1f87c2fcc1d783cb8f946_21d1c25c56" tabindex="-1" value=""></div>
+							    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+							    </div>
+							</form>
+							<!--End mc_embed_signup-->
+						</section>
+						<section class="split contact">
+							<section class="alt">
+								<h3>Address</h3>
+								<p>16205 Wynncrest Ridge Ct<br />
+								Wildwood, MO 63005</p>
+							</section>
+							<section>
+								<h3>Phone</h3>
+								<p>(636) 448-1119</p>
+							</section>
+							<section>
+								<h3>Email</h3>
+								<p><a href="mailto:info.jfkorg@gmail.com">info.jfkorg@gmail.com</a></p>
+							</section>
+							<section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+									<li><a href="https://twitter.com/just__for__kidz" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="https://www.facebook.com/jfkorg/" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="https://www.instagram.com/jfkorg/" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+							</section>
 						</section>
 					</footer>
 
